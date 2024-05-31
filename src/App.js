c3416a40f7d66176de4b1950023287614893ab37
Joanafgg <joanafgg13@gmail.com>
2024-05-31 10:35:38 +0100

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@
 import "./App.css";
 
 export default function App() {
-  return(
-    <div className="App">Weather App</div>
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Joanafgg" target="_blank" rel="noreferrer">
+          Joana Garcia{" "}
+        </a>{" "}
+        and is open-sourced
+        <a
+          href="https://github.com/Joanafgg/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {""} on Github
+        </a>
+      </footer>
+    </div>
   );
 }
-
