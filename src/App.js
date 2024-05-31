66656d839e0c34daebf2245d95a0fe75a86dc250
Joanafgg <joanafgg13@gmail.com>
2024-05-31 13:24:04 +0100

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-/** @format */
-
-import "./App.css";
+import './App.css';
+import React from 'react';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Joanafgg" target="_blank" rel="noreferrer">
-          Joana Garcia{" "}
-        </a>{" "}
-        and is open-sourced
-        <a
-          href="https://github.com/Joanafgg/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {""} on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Joanafgg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joana Garcia{" "}
+          </a>{" "}
+          and is open-sourced
+          <a
+            href="https://github.com/Joanafgg/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
