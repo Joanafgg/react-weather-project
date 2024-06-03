1375a15ae250088d05964aed6e6321311583bab9
Joanafgg <joanafgg13@gmail.com>
2024-06-03 21:46:24 +0100

    Added the WeatherTemperature component with the units convertion

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -30,7 +30,7 @@ export default function WeatherIcon(props) {
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
         color="grey"
-        size={70}
+        size={90}
         animate={true}
       />
     );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,6 +3,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -19,12 +20,14 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon}  />
-          <span className="temperature">
-            {" "}
-            {Math.round(props.data.temperature)}
-          </span>{" "}
-          <span className="unit"> °C</span>
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
